4f7f36c61cd5574e9984f943ced72c688f5fdaa1
Ryan Campbell <rcampbell@loraxcompliance.com>
2024-03-25 11:31:08 +0000

    Added Calculator.cs.

---
+++ InterviewTestQA/InterviewTestAutomation/Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace InterviewTestQA.InterviewTestAutomation
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Cannot divide by zero.");
+            return a / b;
+        }
+
+        public int Square(int a)
+        {
+            return a * a;
+        }
+
+        public int SquareRoot(int a)
+        {
+            if (a == 0)
+                throw new ArgumentException("Cannot square root zero.");
+            return a / a;
+        }
+    }
+}
